f57297094c5fdd5e53e3d26f0824f02065abf2fc
eaglecoffeDev <148896168+eaglecoffeDev@users.noreply.github.com>
2023-12-06 17:01:22 +0100

    sfr.py

--- sfr.py
+++ sfr.py
@@ -1,5 +1,8 @@
 import os
-from scapy.all import IP, TCP, UDP, send, sniff
+import signal
+import threading
+from scapy.all import IP, TCP, UDP, send, sniff, sr1
+import time
 
 class WifiBypass:
     def __init__(self, interface='wlan0', local_ip='192.168.1.100', remote_host='www.sfr.fr', remote_port=80):
@@ -35,13 +38,32 @@ class WifiBypass:
                     packet[UDP].dport = packet[UDP].sport
 
                 send(packet, iface=self.interface, verbose=0)
-            else:
-               
-                pass
+
+    def send_keep_alive(self):
+        ip = IP(dst=self.remote_host)
+        tcp = TCP(dport=self.remote_port, flags="S")
+        send(ip / tcp, iface=self.interface, verbose=0)
+        response = sr1(ip / tcp, timeout=5, verbose=0)
+        if response and response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+            tcp_ack = TCP(dport=self.remote_port, flags="A")
+            send(ip / tcp_ack, iface=self.interface, verbose=0)
 
     def start(self):
         self.set_ip_forward()
+        threading.Thread(target=self.sniff_and_send).start()
+
+        while True:
+            self.send_keep_alive()
+            time.sleep(60) # sleep for 1 minute
+
+    def sniff_and_send(self):
         sniff(iface=self.interface, prn=self.process_packet)
 
-bypass = WifiBypass()
-bypass.start()
+    def signal_handler(self, signum, frame):
+        print('Vous avez arrêté le programme')
+        exit(0)
+
+if __name__ == '__main__':
+    bypass = WifiBypass()
+    signal.signal(signal.SIGINT, bypass.signal_handler)
+    bypass.start()
